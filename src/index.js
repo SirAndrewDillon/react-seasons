626256e65c2c0645c99c46033ec803a8eac1d0ff
Andrew Dillon <majik895@gmail.com>
2019-06-01 15:19:58 -0400

    first react-seasons commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>It's hot in here</div>
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
