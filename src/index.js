2f5fdd505d60a3fd2b590bbc6ac157c9c8280e57
Andrew Dillon <majik895@gmail.com>
2019-06-02 10:10:44 -0400

    refactored index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-window.navigator.geolocation.getCurrentPosition(
-    //Sucsess Callback
-    (position) => console.log(position),
-    //Failure Callback
-    (err) => console.log(err)
-);
 
-    return <div>Latitude:</div>
-}
+class App extends React.Component {
+    //Must always call props with constructor
+    constructor(props) {
+        //super always follows constructor and must have props!
+        //super is a reference to the parent constructor function
+        super(props);
+        //initilizes the STATE object 
+        // THIS IS THE ONLY TIME you do direct assignment to this.state!
+        this.state = { lat: null, errorMessage: '' };
+    }
+
+   componentDidMount() {
+       window.navigator.geolocation.getCurrentPosition(
+         position => this.setState ({lat: position.coords.latitude}),
+         err => this.setState ({errorMessage: err.message})
+      );
+   }
+
+   
+        
+        //Render must be used..React rule to define it!  
+        //Conditional render  
+        render() {
+           if (this.state.errorMessage && !this.state.lat) {
+               return <div>Error: {this.state.errorMessage}</div>;
+           }
+           if (!this.state.errorMessage && this.state.lat) {
+               return <div>Latitude: {this.state.lat}</div>;
+           }
+           return <div>Loading...</div>
+        }
+    }
+   
+    
 
 
 
